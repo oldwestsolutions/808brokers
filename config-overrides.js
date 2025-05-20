b649ccccf8180699fe13fdc5ef6fb033717b6527
oldwestsolutions <hbm.wallet@gmail.com>
2025-05-20 07:24:38 -0500

    fix: add webpack polyfills for crypto and other Node.js core modules

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "buffer": require.resolve("buffer/"),
+    "util": require.resolve("util/"),
+  };
+
+  config.resolve.extensions = [...config.resolve.extensions, ".ts", ".js"];
+  config.ignoreWarnings = [/Failed to parse source map/];
+
+  return config;
+}; 
